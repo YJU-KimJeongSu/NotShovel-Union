f457fe8968c81212f88ff43f4d27e03967e8ad0a
bys096 <82657858+bys096@users.noreply.github.com>
2023-04-01 15:39:18 +0900

    Add draggable to SideBar for changing board order

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -6,70 +6,50 @@
         <i class='bx bx-menu' id="btn" @click="btnClick"></i>
     </div>
     <ul class="nav-list">
-      <li>
-        <i class='bx bx-search' @click="searchClick"></i>
-        <input type="text" placeholder="Search...">
-        <span class="tooltip">Search</span>
-      </li>
-      <li @click="clickBoard('dashMain');">
-        <a href="#">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-        <span class="tooltip">Dashboard</span>
-      </li>
-      <li @click="clickBoard('meetingNotes');">
-        <a href="#">
-        <i class='bx bx-folder' ></i>
-          <span class="links_name">회의록</span>
-        </a>
-        <span class="tooltip">회의록</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-user' ></i>
-          <span class="links_name">User</span>
-        </a>
-        <span class="tooltip">User</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-chat' ></i>
-          <span class="links_name">Messages</span>
-        </a>
-        <span class="tooltip">Messages</span>
-      </li>
-      <li>
-        <a href="#">
-          <i class='bx bx-pie-chart-alt-2' ></i>
-          <span class="links_name">Analytics</span>
-        </a>
-        <span class="tooltip">Analytics</span>
-      </li>
+      <draggable v-model="myList">
+        <li v-for="(board, index) in bList"
+          :key="index"
+          @click="clickBoard(board.clickMethod)"
+        >
+          <a href="#">
+            <i v-bind:class="board.icon"></i>
+            <!-- <i class='bx bx-folder' ></i> -->
+            <span class="links_name">{{board.name}}</span>
+          </a>
+          <span class="tooltip">{{board.name}}</span>
+        </li>
+      </draggable>
       
       <li>
         <a href="#">
-          <i class='bx bx-cog' ></i>
-          <span class="links_name">Setting</span>
+          <i class='bx bx-plus' ></i>
+          <span class="links_name">게시판 추가</span>
         </a>
-        <span class="tooltip">Setting</span>
-      </li>
-      <li class="profile">
-        <div class="profile-details">
-          <img src="../assets/profile.jpg" alt="profileImg">
-          <div class="name_job">
-            <div class="name">Prem Shahi</div>
-            <div class="job">Web designer</div>
-          </div>
-        </div>
-        <i class='bx bx-log-out' id="log_out" ></i>
+        <span class="tooltip">게시판 추가</span>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+
 export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      bList: [
+        {name: '회의록', icon: 'bx bx-folder', clickMethod: 'meetingNotes'},
+        {name: 'User', icon: 'bx bx-user', clickMethod: ''},
+        {name: 'Messages', icon: 'bx bx-chat', clickMethod: ''},
+        {name: 'Gant Chart', icon: 'bx bx-pie-chart-alt-2', clickMethod: ''},
+        {name: 'Setting', icon: 'bx bx-cog', clickMethod: ''},
+        
+      ]
+    }
+  },
   methods: {
     menuBtnChange: function() {
       let sidebar = document.querySelector(".sidebar");
@@ -83,12 +63,12 @@ export default {
     searchClick: function() {
       let sidebar = document.querySelector(".sidebar");
       sidebar.classList.toggle("open");
-      this.menuBtnChange(); //calling the function(optional)
+      this.menuBtnChange();
     },
     btnClick: function() {
       let sidebar = document.querySelector(".sidebar");
       sidebar.classList.toggle("open");
-      this.menuBtnChange();//calling the function(optional)
+      this.menuBtnChange();
     },
 
     //  아이콘 클릭에 따라, 각 이벤트 정보를 담고 DashBoard.vue로 emit
