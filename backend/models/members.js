1143b2cf2f43493cdc0fc197df0ed75e0be4f594
Jungwon  Kim <kimwonny8@gmail.com>
2023-04-04 15:33:06 +0900

    Modify models

--- backend/models/members.js
+++ backend/models/members.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const members = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    phone_number: { type: String, unique: true },
-    profile_picture: String,
-    signup_date: { type: Date, default: Date.now }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  phone_number: { type: String, unique: true },
+  profile_picture: String,
+  signup_date: { type: Date, default: Date.now },
+  project_ids: [{ type: Schema.Types.ObjectId, ref: "projects" }],
+  state: { type: String, default: "1", enum: ["0", "1"] },
 });
 
-module.exports = mongoose.model('members', members);
+module.exports = mongoose.model("members", members);
--- backend/models/projects.js
+++ backend/models/projects.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const projects = new Schema({
-    name: { type: String, required: true },
-    picture: String,
-    description: String,
-    create_date: { type: Date, default: Date.now },
-    member_ids: [{ type: Schema.Types.ObjectId, ref: 'members', required: true }],
-    admin_ids: [{ type: Schema.Types.ObjectId, ref: 'members' }],
-    // board_ids: [
-    //     {
-    //         kind: { type: String, enum: ['gantt_charts', 'meeting_minutes'] }, // enum으로 정해놓은 값 외에 다른 값이 들어오면 에러
-    //         board: { type: Schema.Types.ObjectId, refPath: 'board_ids.kind' },
-    //     },
-    // ]
-    // 이게 사용하기 더 쉬움
-    meeting_minute_ids: [{ type: Schema.Types.ObjectId, ref: 'meeting_minutes' }],
-    gantt_charts_ids: [{ type: Schema.Types.ObjectId, ref: 'gantt_charts' }]
+  name: { type: String, required: true },
+  picture: String,
+  description: String,
+  create_date: { type: Date, default: Date.now },
+  member_ids: [{ type: Schema.Types.ObjectId, ref: "members", required: true }],
+  admin_id: { type: Schema.Types.ObjectId, ref: "members", required: true },
+  manager_ids: [{ type: Schema.Types.ObjectId, ref: "members" }],
+  // board_ids: [
+  //     {
+  //         kind: { type: String, enum: ['gantt_charts', 'meeting_minutes'] }, // enum으로 정해놓은 값 외에 다른 값이 들어오면 에러
+  //         board: { type: Schema.Types.ObjectId, refPath: 'board_ids.kind' },
+  //     },
+  // ]
+  // 이게 사용하기 더 쉬움
+  meeting_minute_ids: [{ type: Schema.Types.ObjectId, ref: "meeting_minutes" }],
+  gantt_charts_ids: [{ type: Schema.Types.ObjectId, ref: "gantt_charts" }],
 });
 
-module.exports = mongoose.model('projects', projects);
+module.exports = mongoose.model("projects", projects);
