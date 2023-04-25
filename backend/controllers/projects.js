404760dd5dcc3241f6dc1f9c9e579a9df91508da
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2023-04-25 10:55:23 +0900

    [Refactor] Backend refactoring
    - 불필요한 코드 삭제
    - then 안에 then이 들어있는 구조 try-catch로 변경
    - await async를 명시하여 가독성 향상
    - return을 명시하여 가독성 향

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,7 +1,6 @@
 const members = require("../models/members");
 const projects = require("../models/projects");
 const multer = require('multer');
-const fs = require('fs');
 const path = require('path');
 
 exports.save = async (req, res) => {
@@ -24,12 +23,13 @@ exports.save = async (req, res) => {
     );
 
     if (member) {
-      res.json({id: project_id}); // 원래 member 객체를 보냈으나, member는 안쓰이는 것 같아서 project_id만 보내게 수정
+      res.json({ id: project_id }); // 원래 member 객체를 보냈으나, member는 안쓰이는 것 같아서 project_id만 보내게 수정
     } else {
-      res.status(404).json({ error: '회원을 찾을 수 없습니다.'  });
+      res.status(404).send();
     }
   } catch (err) {
-    res.status(400).send(err);
+    console.log(err);
+    return res.status(400).send(err);
   }
 };
 
@@ -61,7 +61,7 @@ exports.findProjects = async (req, res) => {
         }
       }
       // console.log(projectData);
-      res.json(projectData);
+      return res.json(projectData);
     } else {
       return res.status(404).json({ error: '회원을 찾을 수 없습니다.' });
     }
@@ -71,155 +71,136 @@ exports.findProjects = async (req, res) => {
 };
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/images');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({ storage: storage });
 
 exports.imageUpload = (req, res) => {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
+  const upload = multer({ storage: storage });
+
   upload.single('image')(req, res, function (err) {
     if (err) {
       return res.status(400).json({ message: '이미지 업로드 실패' });
     }
     const { filename, mimetype, size } = req.file;
     return res.json({ filename: filename });
-    
+
   });
 };
 
-exports.findAuth = async(req, res, next) => {
+exports.findAuth = async (req, res, next) => {
   try {
     const member_id = req.query.member_id;
-    console.log(`user id: ${member_id}`);
+    // console.log(`user id: ${member_id}`);
     // console.log('findAuth 실행중');
     // const member = await projects.findById(member_id, "member_ids");
-    const project = await projects.findOne({member_ids : member_id});
-    
+    const project = await projects.findOne({ member_ids: member_id });
+
     if (project) {
       const admin = project.admin_id;
       const managers = project.manager_ids;
-      console.log(`admin: ${admin}`);
-      console.log(`manager: ${managers}`);
+      // console.log(`admin: ${admin}`);
+      // console.log(`manager: ${managers}`);
 
-      const jsonData = {admin_id: admin, manager_ids: []};
+      const jsonData = { admin_id: admin, manager_ids: [] };
       for (const manager of managers) {
         jsonData.manager_ids.push(manager);
       }
-      res.json(jsonData);
-    } else {console.log('가져오기 실패');}
-  } catch(err) {
+      return res.json(jsonData);
+    } else { console.log('가져오기 실패'); }
+  } catch (err) {
     return res.status(400).send(err);
   }
 };
 
-exports.updateProject = async(req,res) => {
+exports.updateProject = async (req, res) => {
   try {
-    const { name, description, image, project_id } = req.body; 
+    const { name, description, image, project_id } = req.body;
 
     const updatedProject = await projects.findByIdAndUpdate(project_id, {
       name,
       description,
       image
-    }, { new: true }); 
-    
+    }, { new: true });
+
     if (updatedProject) {
       return res.status(200).json(updatedProject);
     } else {
       return res.status(404).json({ error: '프로젝트를 찾을 수 없습니다.' });
     }
   } catch (err) {
-      return res.status(500).send(err);
+    console.log(err);
+    return res.status(500).send(err);
   }
 };
 
-exports.deleteProject = async(req,res) => {
+exports.deleteProject = async (req, res) => {
   try {
-    const project_id = req.body.project_id; 
+    const project_id = req.body.project_id;
 
     const deleteProject = await projects.deleteOne({ _id: project_id });
-    if (deleteProject.deletedCount > 0) { 
+    if (deleteProject.deletedCount > 0) {
       return res.status(200).json({ message: '프로젝트가 삭제되었습니다.' });
     } else {
       return res.status(404).json({ error: '프로젝트를 찾을 수 없습니다.' });
     }
   } catch (err) {
-      return res.status(500).send(err);
+    console.log(err);
+    return res.status(500).send(err);
   }
-};
-
-exports.findMembers = async(req, res) => {
-  const project_id = req.query.project_id;
-  const member_ids = await projects.findOne({project_id : project_id});
 };
 
 exports.exitProject = async (req, res) => {
   const { member_id, project_id } = req.body;
-  await projects.findOne({ _id: project_id })
-    .then(async (data) => {
-      if (member_id == data.admin_id) { // 어드민은 프로젝트 탈퇴 불가능
-        return res.status(403).send('admin');
-      } else { // 어드민이 아닐 때는 그냥 탈퇴
-        const filter = { _id: member_id };
-        const update = {
-          $pull: {
-            project_ids: { $in: project_id }
-          }
-        };
-        const option = { new: true }
-        await members.findOneAndUpdate(filter, update, option)
-          .then(async () => {
-            const filter2 = { _id: project_id };
-            const update2 = {
-              $pull: {
-                manager_ids: { $in: member_id },
-                member_ids: { $in: member_id }
-              }
-            };
-            const option2 = { new: true }
-            await projects.findOneAndUpdate(filter2, update2, option2)
-              .then(() => {
-                return res.status(200).send('exit');
-              })
-              .catch((err) => {
-                console.log(err);
-                return res.status(500).send();
-              })
-          })
-          .catch((err) => {
-            console.log(err);
-            return res.status(500).send();
-          })
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send();
-    })
+
+  try {
+    const data = await projects.findOne({ _id: project_id });
+
+    if (member_id == data.admin_id) { // 어드민은 탈퇴 불가능
+      return res.status(403).send('admin');
+    } else { // 어드민이 아니면 그냥 탈퇴
+      const filter = { _id: member_id };
+      const update = {
+        $pull: {
+          project_ids: { $in: project_id }
+        }
+      };
+      const option = { new: true };
+
+      await members.findOneAndUpdate(filter, update, option);
+
+      const filter2 = { _id: project_id };
+      const update2 = {
+        $pull: {
+          manager_ids: { $in: member_id },
+          member_ids: { $in: member_id }
+        }
+      };
+      const option2 = { new: true };
+
+      await projects.findOneAndUpdate(filter2, update2, option2);
+      return res.status(200).send('exit');
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
 };
 
 exports.registerToProject = async (req, res) => {
-  let checker = false;
   const { member_id, project_id } = req.body;
-  const project = await projects.findOne({ _id: project_id })
-    .then((data) => {
-      if (data.member_ids.includes(member_id)) {
-        return res.status(409).send('already exists');
-      } else {
-        checker = true;
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send(err);
-    });
+  try {
+    const project = await projects.findOne({ _id: project_id });
 
-  if (checker) {
+    if (project.member_ids.includes(member_id)) {
+      return res.status(409).send('already exists');
+    }
+
     const filter = { _id: project_id };
     const update = { $push: { member_ids: member_id } };
     const option = { new: true };
@@ -228,13 +209,11 @@ exports.registerToProject = async (req, res) => {
     const filter2 = { _id: member_id };
     const update2 = { $push: { project_ids: project_id } };
     const option2 = { new: true };
-    await members.findOneAndUpdate(filter2, update2, option2)
-      .then(() => {
-        return res.status(200).send();
-      })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).send(err);
-      })
+    await members.findOneAndUpdate(filter2, update2, option2);
+
+    return res.status(200).send();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
   }
 };
