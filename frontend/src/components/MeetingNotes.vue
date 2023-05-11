aa1cf88a7ff2eeb5e137538c8525425099617076
Jungwon  Kim <kimwonny8@gmail.com>
2023-05-11 17:09:04 +0900

    [Add] Meeting note editor

--- frontend/src/components/MeetingNotes.vue
+++ frontend/src/components/MeetingNotes.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="section" v-bind:class="{open: props}">
-    <div class="select-menu">
+  <div class="section" v-bind:class="{ open: props }">
+    <!-- 글 작성 -->
+    <div class="meeting-form" v-show="isWrite">
+      <MeetingNoteEditor :isWrite="this.isWrite"></MeetingNoteEditor>
+    </div>
+    <!-- 글 목록 -->
+    <div class="meeting-form" v-show="!isWrite">
+      <div class="select-menu">
         <div class="select-btn" @click="clickSelectBtn">
           <span class="sBtn-text">Select your option</span>
           <i class="bx bx-chevron-down"></i>
         </div>
-      
         <ul class="options">
           <li class="option">
             <i class="bx bxl-github" style="color: #171515;"></i>
@@ -15,30 +20,17 @@
             <i class="bx bxl-instagram-alt" style="color: #E1306C;"></i>
             <span class="option-text">Instagram</span>
           </li>
-          <!-- <li class="option">
-            <i class="bx bxl-linkedin-square" style="color: #0E76A8;"></i>
-            <span class="option-text">Linkedin</span>
-          </li>
-          <li class="option">
-            <i class="bx bxl-facebook-circle" style="color: #4267B2;"></i>
-            <span class="option-text">Facebook</span>
-          </li>
-          <li class="option">
-            <i class="bx bxl-twitter" style="color: #1DA1F2;"></i>
-            <span class="option-text">Twitter</span>
-          </li> -->
         </ul>
       </div>
-
-
-    <table height="200px">
+      <button type="button" class="btn btn-outline-secondary" @click="goEditor()">글 작성</button>
+      <table height="200px">
         <thead>
           <tr>
             <th colspan="">No.</th>
             <th colspan="">Title</th>
             <th colspan="" id="x">Date</th>
           </tr>
-          
+
         </thead>
         <tbody>
           <tr>
@@ -46,7 +38,7 @@
             <td>Start Union Project!</td>
             <td>
               <i class="material-icons button edit">2022-03-07</i>
-              
+
             </td>
           </tr>
           <tr>
@@ -65,20 +57,33 @@
           </tr>
         </tbody>
       </table>
+    </div>
   </div>
 </template>
 
 <script>
+import meetingNoteEditor from "@/components/MeetingNoteEditor.vue"
 
 export default {
+  data() {
+    return {
+      isWrite: false,
+    }
+  },
   props: ['props'],
+  components: {
+    MeetingNoteEditor: meetingNoteEditor
+  },
   methods: {
-    clickSelectBtn: function() {
+    goEditor(){
+      this.isWrite=true;
+    },
+    clickSelectBtn: function () {
       const optionMenu = document.querySelector(".select-menu");
       const options = optionMenu.querySelectorAll(".option");
       optionMenu.classList.toggle("active");
-      options.forEach(option =>{
-        option.addEventListener("click", ()=> {
+      options.forEach(option => {
+        option.addEventListener("click", () => {
           let selectedOption = option.querySelector(".option-text").innerText;
           sBtn_text.innerText = selectedOption;
           optionMenu.classList.remove("active");
@@ -95,6 +100,12 @@ export default {
 @import url('../assets/css/selectStyle.css');
 @import url('https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css');
 
+.meeting-form{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 .section {
   position: relative;
   animation-name: close;
@@ -113,17 +124,19 @@ export default {
   from {
     left: 0;
   }
+
   to {
     left: 9%;
   }
 }
+
 @keyframes close {
   from {
     left: 9%;
   }
+
   to {
     left: 0;
   }
 }
-
 </style>
