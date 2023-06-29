08746744b6faa3bd42e67b16e36991d476c14bb9
Jungwon  Kim <kimwonny8@gmail.com>
2023-06-29 11:19:00 +0900

    [Add] apiLimiter

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ const boardController= require('../controllers/common/board');
 const meetingController = require('../controllers/meeting_minutes');
 const chatController = require('../controllers/chattings');
 const s3Controller = require('../controllers/s3');
-const { isLoggedIn, isNotLoggedIn, verifyToken } = require('../middlewares');
+const { isNotLoggedIn, verifyToken, apiLimiter } = require('../middlewares');
 
 module.exports = (app) => {
   //auth
@@ -14,53 +14,52 @@ module.exports = (app) => {
   app.post('/api/auth/signin', isNotLoggedIn, authController.signIn);
   
   //member
-  app.patch('/api/member/edit',  verifyToken, memberController.editMember);
-  app.delete('/api/member/delete', verifyToken, memberController.deleteMemeber);
-  // app.post('/api/member/image', verifyToken, memberController.imageUpload);
-  app.post('/api/member/chkPw', verifyToken, memberController.chkPW);
-  app.get('/api/member/emailAuth', memberController.emailAuth);
-  app.get('/api/member/chkEmail', isNotLoggedIn, memberController.chkEmail);
-  app.patch('/api/member/passwordReset', isNotLoggedIn, memberController.passwordReset);
+  app.patch('/api/member/edit',  verifyToken, apiLimiter, memberController.editMember);
+  app.delete('/api/member/delete', verifyToken, apiLimiter, memberController.deleteMemeber);
+  app.post('/api/member/chkPw', verifyToken, apiLimiter, memberController.chkPW);
+  app.get('/api/member/emailAuth', apiLimiter, memberController.emailAuth);
+  app.get('/api/member/chkEmail', isNotLoggedIn, apiLimiter, memberController.chkEmail);
+  app.patch('/api/member/passwordReset', isNotLoggedIn, apiLimiter, memberController.passwordReset);
 
   // project
-  app.post('/api/project', verifyToken, projectController.save);
-  app.get('/api/project', verifyToken, projectController.findProjects);
-  app.patch('/api/project', verifyToken, projectController.updateProject);
-  app.delete('/api/project', verifyToken, projectController.deleteProject);
+  app.post('/api/project', verifyToken, apiLimiter, projectController.save);
+  app.get('/api/project', verifyToken, apiLimiter, projectController.findProjects);
+  app.patch('/api/project', verifyToken, apiLimiter, projectController.updateProject);
+  app.delete('/api/project', verifyToken, apiLimiter, projectController.deleteProject);
   
   // app.post('/api/project/upload', projectController.imageUpload);
-  app.get('/api/project/authority', verifyToken, projectController.findAuth);
+  app.get('/api/project/authority', verifyToken, apiLimiter, projectController.findAuth);
 
-  app.get('/api/project/members', verifyToken, projectController.findMembers);
-  app.post('/api/project/grade', verifyToken, projectController.changeGrade);
-  app.delete('/api/project/exit', verifyToken, projectController.exitProject);
-  app.patch('/api/project/register', verifyToken, projectController.registerToProject);
+  app.get('/api/project/members', verifyToken, apiLimiter, projectController.findMembers);
+  app.post('/api/project/grade', verifyToken, apiLimiter, projectController.changeGrade);
+  app.delete('/api/project/exit', verifyToken, apiLimiter, projectController.exitProject);
+  app.patch('/api/project/register', verifyToken, apiLimiter, projectController.registerToProject);
   
   // gantt
-  app.post('/api/gantt', verifyToken, ganttController.save);
-  app.get('/api/gantt/:board_id', verifyToken, ganttController.getGanttData);
+  app.post('/api/gantt', verifyToken, apiLimiter, ganttController.save);
+  app.get('/api/gantt/:board_id', verifyToken, apiLimiter, ganttController.getGanttData);
 
   // board
-  app.patch('/api/board/order', verifyToken, boardController.changeBoardOrder);
-  app.post('/api/board', verifyToken, boardController.createBoard);
-  app.get('/api/board', verifyToken, boardController.getBoardList);
-  app.delete('/api/board/:bid/:btype', boardController.deleteBoard);
-  app.patch('/api/board', boardController.updateBoard);
+  app.patch('/api/board/order', verifyToken, apiLimiter, boardController.changeBoardOrder);
+  app.post('/api/board', verifyToken, apiLimiter, boardController.createBoard);
+  app.get('/api/board', verifyToken, apiLimiter, boardController.getBoardList);
+  app.delete('/api/board/:bid/:btype', apiLimiter, boardController.deleteBoard);
+  app.patch('/api/board', apiLimiter, boardController.updateBoard);
 
   
   // meeting_minutes
-  app.post('/api/meeting', verifyToken, meetingController.save);
-  app.patch('/api/meeting', verifyToken, meetingController.update);
-  app.delete('/api/meeting', verifyToken, meetingController.deleteMeetingMinute);
-  app.get('/api/meeting/chat', verifyToken, meetingController.getMinuteChattings);
-  app.get('/api/meeting/:id', verifyToken, meetingController.getAllList);
-  app.get('/api/meeting/:boardId/:id', verifyToken, meetingController.getDetailMeetingMinute);
-  app.post('/api/meeting/chat', verifyToken, meetingController.saveMinuteChattings);
+  app.post('/api/meeting', verifyToken, apiLimiter, meetingController.save);
+  app.patch('/api/meeting', verifyToken, apiLimiter, meetingController.update);
+  app.delete('/api/meeting', verifyToken, apiLimiter, meetingController.deleteMeetingMinute);
+  app.get('/api/meeting/chat', verifyToken, apiLimiter, meetingController.getMinuteChattings);
+  app.get('/api/meeting/:id', verifyToken, apiLimiter, meetingController.getAllList);
+  app.get('/api/meeting/:boardId/:id', verifyToken, apiLimiter, meetingController.getDetailMeetingMinute);
+  app.post('/api/meeting/chat', verifyToken, apiLimiter, meetingController.saveMinuteChattings);
 
   // chattings
-  app.post('/api/chat', verifyToken, chatController.insertChat);
-  app.get('/api/chat/list', verifyToken, chatController.getChatList);
+  app.post('/api/chat', verifyToken, apiLimiter, chatController.insertChat);
+  app.get('/api/chat/list', verifyToken, apiLimiter, chatController.getChatList);
 
   // s3
-  app.get('/api/s3/url', verifyToken, s3Controller.getPresignedURL);
+  app.get('/api/s3/url', verifyToken, apiLimiter, s3Controller.getPresignedURL);
 };
